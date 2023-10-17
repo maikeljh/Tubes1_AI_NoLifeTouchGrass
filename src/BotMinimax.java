e3c14748f23c2f8e140c184b3a29c668bcedfc62
Raylouis <sebastianraynard@gmail.com>
2023-10-17 10:26:11 +0700

    feat: add comment botMinimax

--- src/BotMinimax.java
+++ src/BotMinimax.java
@@ -6,22 +6,26 @@ import javafx.util.Pair;
 public class BotMinimax extends Bot {
     private long startTime;
 
+    //Minimax algorithm
     public int minimax_search(char[][] board, int depth, int alpha, int beta, int playerXScore, int playerOScore, boolean botTurn){
         int[] tempScore;
         int row = board.length;
         int col = board[0].length;
 
-
+        // Limiting depth
         if (depth == 4){
             return objective_function(playerXScore, playerOScore);
         }
 
+        //Maximizing
         if (botTurn){
             int bestScore = Integer.MIN_VALUE;
 
             for (int i = 0; i < row; i++){
                 for (int j = 0; j< col; j++){
                     if (board[i][j] == ' '){
+
+                        // Copy board
                         char[][] tempBoard = new char[row][col];
                         for (int a = 0; a < row; a++){
                             System.arraycopy(board[a], 0, tempBoard[a], 0, col);
@@ -39,6 +43,7 @@ public class BotMinimax extends Bot {
                         bestScore = Math.max(score, bestScore);
                         alpha = Math.max(alpha, bestScore);
 
+                        //alpha beta pruning
                         if (alpha >= beta){
                             break;
                         }
@@ -47,12 +52,15 @@ public class BotMinimax extends Bot {
             }
             return bestScore;
         }
+        //Minimizing
         else {
             int bestScore = Integer.MAX_VALUE;
 
             for (int i = 0; i < row; i++){
                 for (int j = 0; j< col; j++){
                     if (board[i][j] == ' '){
+
+                        //copy board
                         char[][] tempBoard = new char[row][col];
                         for (int a = 0; a < row; a++){
                             System.arraycopy(board[a], 0, tempBoard[a], 0, col);
@@ -70,6 +78,7 @@ public class BotMinimax extends Bot {
                         bestScore = Math.min(score, bestScore);
                         beta = Math.min(beta, bestScore);
 
+                        //alpha beta pruning
                         if (alpha >= beta){
                             break;
                         }
@@ -88,6 +97,7 @@ public class BotMinimax extends Bot {
         int row = board.length;
         int col = board[0].length;
 
+        //fill board
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 String buttonText = buttons[i][j].getText();
@@ -102,6 +112,8 @@ public class BotMinimax extends Bot {
         for (int i = 0; i < row; i++){
             for (int j = 0; j< col; j++){
                 if (board[i][j] == ' '){
+
+                    //copy board
                     char[][] tempBoard = new char[row][col];
                     for (int a = 0; a < row; a++){
                         System.arraycopy(board[a], 0, tempBoard[a], 0, col);
@@ -115,8 +127,10 @@ public class BotMinimax extends Bot {
                     playerOScore = tempScore[0];
                     playerXScore = tempScore[1];
                     
+                    //do minimax search
                     int score = minimax_search(tempBoard, 0, Integer.MIN_VALUE, Integer.MAX_VALUE, playerXScore, playerOScore, false);
 
+                    //get best move with highest score
                     if (score > bestScore) {
                         bestScore = score;
                         bestMove[0] = i;
@@ -134,6 +148,7 @@ public class BotMinimax extends Bot {
         int scoreBot = 0;
         int[] score = new int[2];
     
+        // calculate bot and player score
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 if (board[i][j] == 'X') {
