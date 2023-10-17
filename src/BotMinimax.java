2866f2346643b365a78ff03b10a6c0ba68749409
Raylouis <sebastianraynard@gmail.com>
2023-10-17 10:21:42 +0700

    feat: fix botMinimax minor bug

--- src/BotMinimax.java
+++ src/BotMinimax.java
@@ -8,8 +8,6 @@ public class BotMinimax extends Bot {
 
     public int minimax_search(char[][] board, int depth, int alpha, int beta, int playerXScore, int playerOScore, boolean botTurn){
         int[] tempScore;
-        // int objectiveFunctionValue = objective_function(playerXScore, playerOScore);
-        // int evaluateValue = evaluate(board, playerXScore, playerOScore);
         int row = board.length;
         int col = board[0].length;
 
@@ -109,7 +107,7 @@ public class BotMinimax extends Bot {
                         System.arraycopy(board[a], 0, tempBoard[a], 0, col);
                     }
 
-                    tempBoard[i][j] = 'X';
+                    tempBoard[i][j] = 'O';
                     Pair currentCoor = new Pair(i, j);
                     checkAdjacency(tempBoard, currentCoor, "Bot");
 
@@ -149,27 +147,5 @@ public class BotMinimax extends Bot {
         score[1] = scorePlayer;
         return score;
     }
-
-    // public int countBoard(char[][] board) {
-    //     int count = 0;
-    //     for (int i = 0; i < board.length; i++) {
-    //         for (int j = 0; j < board[0].length; j++) {
-    //             if (board[i][j] == 'O') {
-    //                 count++;
-    //             }
-    //         }
-    //     }
-    //     return count;
-    // }
-
-    // public int evaluate(char[][] board, int playerXScore, int playerOScore){
-    //     int scoreWeight = 1;
-    //     int controlWeight = 1;
-
-    //     int scoreDifference = objective_function(playerXScore, playerOScore);
-    //     int boardControl = countBoard(board);
-
-    //     return (scoreWeight * scoreDifference) + (controlWeight * boardControl);
-    // }
 }
     
